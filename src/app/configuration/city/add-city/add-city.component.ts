80a4b3395b2cdd388b528227637942d6927d611c
Xani595 <itszk6@gmail.com>
2021-03-05 12:42:29 +0500

    City Changes

--- src/app/configuration/city/add-city/add-city.component.ts
+++ src/app/configuration/city/add-city/add-city.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-add-city',
@@ -6,10 +10,81 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-city.component.css']
 })
 export class AddCityComponent implements OnInit {
+  country:any=[];
+  countryId:null;
+  response:any;
   data:any={};
-  constructor() { }
+
+  constructor(private http:HttpClient,
+    private toastr: ToastrService,
+    private _NgbActiveModal: NgbActiveModal) { }
 
   ngOnInit(): void {
+    this.getCountry();
   }
+
+    
+  get activeModal() {
+    return this._NgbActiveModal;
+  }
+
+
+  getCountry()
+  {
+    this.http.get(`${environment.apiUrl}/api/Lookups/Countries`)
+    .subscribe(
+      res=> { 
+        this.response = res;
+        if (this.response.success == true){
+          this.country =this.response.data;
+        }
+        else {
+          this.toastr.error('Something went Worng', 'Message.');
+            }
+
+      }, err => { 
+        if (err.status == 400) {
+          this.toastr.error('Something went Worng', 'Message.');
+        }
+      });
+  }
+
+
+
+  
+  addCity()
+  {
+    let varr=  {
+      "name": this.data.name,
+      "details":this.data.details,
+      "countryId":this.countryId,
+      "active":this.data.active
+     
+    }
+
+    this.http.
+    post(`${environment.apiUrl}/api/Configs/AddCity`,varr)
+    .subscribe(
+      res=> { 
+  
+        this.response = res;
+        if (this.response.success == true){
+          this.toastr.success(this.response.message, 'Message.');
+      
+          // this.buyerForm.reset();
+          this.activeModal.close(true);
+        }
+        else {
+          this.toastr.error('Something went Worng', 'Message.');
+            }
+
+      }, err => {
+        if (err.status == 400) {
+          this.toastr.error('Something went Worng', 'Message.');
+        }
+      });
+  }
+
+
 
 }
--- src/app/configuration/city/city.component.ts
+++ src/app/configuration/city/city.component.ts
@@ -12,11 +12,13 @@ import { EditCityComponent } from './edit-city/edit-city.component';
   styleUrls: ['./city.component.css']
 })
 export class CityComponent implements OnInit {
+  listCount: number;
   response:any;
   rows:any=[];
+  // countryId= null;
+
   columns:any=[];
   data:any={};
-  listCount: number;
   myDate=Date.now();
 
   constructor(private http:HttpClient,
@@ -26,17 +28,20 @@ export class CityComponent implements OnInit {
   ngOnInit(): void {
     this.fetch((data) => {
       this.rows = data;
+      this.listCount= this.rows.length;
     });
+  
   }
 
 
+  
   fetch(cb) {
     let that = this;
     that.http
-    .get(`${environment.apiUrl}/api/Lookups/Countries`)
+    .get(`${environment.apiUrl}/api/Configs/GetAllCity`)
     .subscribe(res => {
       this.response = res;
-      this.listCount = this.fetch.length;
+     
     if(this.response.success==true)
     {
     that.data =this.response.data;
@@ -55,11 +60,35 @@ export class CityComponent implements OnInit {
   }
 
 
+  deleteCity(id)
+  {
+    this.http.delete(`${environment.apiUrl}/api/Configs/DeleteCity/`+id.id )
+    .subscribe(
+      res=> { 
+        this.response = res;
+        if (this.response.success == true){
+         this.toastr.error(this.response.message, 'Message.');
+         this.fetch((data) => {
+          this.rows = data;
+        });
 
+        }
+        else {
+          this.toastr.error('Something went Worng', 'Message.');
+            }
  
+      }, err => {
+        if (err.status == 400) {
+          this.toastr.error(this.response.message, 'Message.');
+        }
+      });
+  }
 
 
-  addCityForm(){
+
+
+  
+  addCity(){
     const modalRef = this.modalService.open(AddCityComponent, { centered: true });
           modalRef.result.then((data) => {
          // on close
@@ -67,6 +96,7 @@ export class CityComponent implements OnInit {
           //  this.date = this.myDate;
            this.fetch((data) => {
             this.rows = data;
+        
           });
            
   
@@ -77,14 +107,16 @@ export class CityComponent implements OnInit {
   } 
   
 
-  editCityForm(){
+  editCity(row){
     const modalRef = this.modalService.open(EditCityComponent, { centered: true });
+    modalRef.componentInstance.userId =row.id;
           modalRef.result.then((data) => {
          // on close
           if(data ==true){
           //  this.date = this.myDate;
            this.fetch((data) => {
             this.rows = data;
+            
           });
            
          }
@@ -92,8 +124,5 @@ export class CityComponent implements OnInit {
          // on dismiss
        });
   } 
-  
-
-
-
 }
+
